7405255b970ca1856dbb299b2e8f3e15f0652a86
Boya Lin <148045657+Mciqk@users.noreply.github.com>
2023-10-16 22:23:38 +0200

    Update person.py

--- MA4/person.py
+++ MA4/person.py
@@ -1,22 +1,12 @@
-""" Python interface to the C++ Person class """
 import ctypes
-lib = ctypes.cdll.LoadLibrary('./libperson.so')
 
-class Person(object):
-	def __init__(self, age):
-		lib.Person_new.argtypes = [ctypes.c_int]
-		lib.Person_new.restype = ctypes.c_void_p
-		lib.Person_get.argtypes = [ctypes.c_void_p]
-		lib.Person_get.restype = ctypes.c_int
-		lib.Person_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
-		lib.Person_delete.argtypes = [ctypes.c_void_p]
-		self.obj = lib.Person_new(age)
+# Load the shared library
+libfib = ctypes.CDLL('./libfibonacci.so')
 
-	def get(self):
-		return lib.Person_get(self.obj)
+# Define the argument and return types
+libfib.fib.argtypes = [ctypes.c_int]
+libfib.fib.restype = ctypes.c_longlong
 
-	def set(self, age):
-		lib.Person_set(self.obj, age)
-        
-	def __del__(self):
-		return lib.Person_delete(self.obj)
+n = 47
+result = libfib.fib(n)
+print(f"Fibonacci({n}) = {result}")
