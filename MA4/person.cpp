a98c41ae648767b4cb88f9300be55b5d9c91040e
Boya Lin <148045657+Mciqk@users.noreply.github.com>
2023-10-16 22:22:14 +0200

    Update person.cpp

--- MA4/person.cpp
+++ MA4/person.cpp
@@ -1,69 +1,31 @@
+// fib.cpp
 #include <cstdlib>
-#include <iostream>
-// Person class 
 
 class Person {
 public:
-    Person(int);
-    int get();
-    void set(int);
-    double getDecades();
-    long long int fib();
+    Person(int n) : n(n) {}
+
+    long long fib() {
+        if (n <= 1) {
+            return n;
+        }
+
+        long long a = 0, b = 1;
+        for (int i = 2; i <= n; ++i) {
+            long long temp = b;
+            b = a + b;
+            a = temp;
+        }
+
+        return b;
+    }
+
 private:
-    int age;
-    long long int _fib(long long int n);
+    int n;
 };
 
- Person::Person(int n) {
-    age = n;
-}
-
-int Person::get() {
-    return age;
-}
-
-void Person::set(int n) {
-    age = n;
-}
-
-double Person::getDecades() {
-    return static_cast<double>(age) / 10.0;
-}
-
-long long int Person::fib() {
-    return _fib(age);
-}
-
-long long int Person::_fib(long long int n) {
-    if (n <= 1) {
-        return n;
-    }
-    else {
-        return _fib(n - 1) + _fib(n - 2);
-    }
-}
-
 extern "C" {
-    Person* Person_new(int n) {
-        return new Person(n);
-    }
-
-    int Person_get(Person* person) {
-        return person->get();
-    }
-
-    void Person_set(Person* person, int n) {
-        person->set(n);
-    }
-
-    long long int Person_fib(Person* person) {
-        return person->fib();
-    }
-
-    void Person_delete(Person* person) {
-        if (person) {
-            delete person;
-        }
-    }
+    Person* Person_new(int n) { return new Person(n); }
+    long long Person_fib(Person* person) { return person->fib(); }
+    void Person_delete(Person* person) { delete person; }
 }
-
